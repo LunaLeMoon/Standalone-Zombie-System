8ae9f59e8973740ae6c821a56d153841996e4dd6
Le Moon Studio <97724060+LunaLeMoon@users.noreply.github.com>
2023-06-02 03:22:17 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 games { 'gta5' }
 
-author 'Weponz'
-description 'Standalone Zombie System'
-version '1.0.2'
+author 'LeMoon Studio, WeponzTV'
+description 'Modified Standalone Zombie System'
+version 'v1.0.0-alpha'
 
 client_script 'zombies.lua'
